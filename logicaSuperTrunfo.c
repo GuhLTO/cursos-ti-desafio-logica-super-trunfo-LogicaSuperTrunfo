5f094e9566b3ac1b58d2cc9a066c7fbab5800c52
Gustavo Luiz Teixeira de Oliveira <gustavoluizteixeiradeoliveira@gmail.com>
2025-09-19 00:06:12 +0000

    criei o scanf

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-
 int main() {
-    printf("Desafio Supertrunfo");
+
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosturisticos1, pontosturisticos2;
+
+    printf("\n=====Desafio Supertrunfo=====\n");
+    
+    printf("Nome do estado: \n");
+     printf("são paulo \n");
+    printf("Codigo da carta: \n");
+     printf("40028922 \n");
+    printf("nome da cidade: \n");
+     printf("Santo andré \n");
+    printf("Ensira o tamanho da sua população: \n");
+     scanf("%i", &populacao1);
+    printf("ensira a area (em km²): \n");
+     scanf("%f", &area1);
+    printf("ensira o seu pib: \n");
+     scanf("%f", &pib1);
+    printf("ensira a quantidade de pontos turisticos: \n");
+     scanf("%i", &pontosturisticos1);
    
  
